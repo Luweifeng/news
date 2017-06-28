1c84214b9129501f924a6cf16674194c637b9f17
Luweifeng <942175634@qq.com>
2017-06-28 20:45:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/feng/news/data/TempPoint.java
@@ -0,0 +1,17 @@
+package com.example.feng.news.data;
+
+/**
+ * Created by yls on 2017/6/1.
+ */
+
+public class TempPoint {
+    public int x;
+    public int lowy;
+    public int highy;
+
+    public TempPoint(int x, int lowy, int highy) {
+        this.x = x;
+        this.lowy = lowy;
+        this.highy = highy;
+    }
+}
